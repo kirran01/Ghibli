36253e7a9afcd861b16ae2e3cc37d124a6f18e78
kirran01 <Kirran-@live.com>
2023-02-07 11:33:31 -0400

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,15 +14,12 @@ const signup = (req, res) => {
     res.status(400).json({ message: "Provide a valid email address." });
     return;
   }
-
   const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!passwordRegex.test(password)) {
-    res
-      .status(400)
-      .json({
-        message:
-          "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
-      });
+    res.status(400).json({
+      message:
+        "Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.",
+    });
     return;
   }
   bcryptjs.hash(password, 10).then((hashedPassword) => {
@@ -41,4 +38,47 @@ const signup = (req, res) => {
   });
 };
 
-module.exports = { signup };
+const login = (req, res) => {
+  const { username, password } = req.body;
+  if (!req.body.username || !req.body.password) {
+    return res.json({
+      error: {
+        message: "fields are blank",
+      },
+    });
+  }
+  let theUser;
+  User.findOne({
+    username,
+  })
+    .then((foundUser) => {
+      if (!foundUser) {
+        return Promise.reject("invalid credentials");
+      }
+      theUser = foundUser;
+      console.log(foundUser);
+      return bcryptjs.compare(password, foundUser.password);
+    })
+    .then((isValidPassword) => {
+      if (!isValidPassword) {
+        return Promise.reject("invalid credentials");
+      }
+      const payload = {
+        _id: theUser._id,
+      };
+      console.log(payload, "<---payload");
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: "72h",
+      });
+      res.json({
+        authToken: authToken,
+        hi: "hello",
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+module.exports = { signup, login };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../controllers/auth.controller");
+const { signup, login } = require("../controllers/auth.controller");
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
